5b934ac4138e23e63f71cdf2a3d993a5b27df0f1
Patric Kenneth <patrickenneth77@gmail.com>
2021-10-30 13:32:31 -0300

    add info

--- src/components/pageLayout/index.js
+++ src/components/pageLayout/index.js
@@ -1,15 +1,32 @@
-import { Box } from '@material-ui/core';
-import React from 'react';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
+import React, { useState } from 'react';
 import Content from '../content';
 import Header from '../header';
 import Footer from '../footer';
 
 function PageLayout() {
+     const [open, setOpen] = useState(true);
+     const handleClose = () => setOpen(false);
      return (
           <Box style={{ padding: '24px 40px 40px 40px', background: '#dedede' }}>
                <Header />
                <Content />
                <Footer />
+               <Dialog open={open} onClose={handleClose} aria-labelledby='form-dialog-title'>
+                    <DialogTitle id='form-dialog-title'>Informação</DialogTitle>
+                    <DialogContent>
+                         <DialogContentText>
+                              Por falta de venda dos numeros o sorteio sera remarcado. Assim que vender todos 
+                              os numeros sera marcado a data do sorteio! Desculpa o transtorno!
+                              Qualquer dúvida entrar em contato. Obrigado pela compreenssão!
+                         </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                         <Button onClick={handleClose} color='secondary'>
+                         Fechar
+                         </Button>
+                    </DialogActions>
+               </Dialog>
           </Box>
      )
 }
